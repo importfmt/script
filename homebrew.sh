5440b85f613077ea6621ac48f916bacdfbb7730b
26n119e <yuxin.weng@outlook.com>
2022-09-17 20:28:24 +0800

    chore: add a shell file for homebrew.

---
+++ homebrew.sh
@@ -0,0 +1,16 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew tap beeftornado/rmtree
+
+brew install clashx
+brew install go
+brew install cmake
+brew install ffmpeg
+brew install unar
+brew install --casks docker
+brew install --casks wechat
+brew install --casks tencent-lemon
+brew install fliqlo
+brew install wpsoffice
+brew install qq
+# brew install gdb
